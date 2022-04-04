2667a515571302aa930f50f39d50923fbe0d8742
buivietnhat <buivietnhatctqb@gmail.com>
2022-04-04 08:21:25 +0700

    finish open-close principle

--- SOLID/Open-Closed/main.cpp
+++ SOLID/Open-Closed/main.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-enum class Color { Red, Green, Blue };
-enum class Size { Small, Medium, Large };
+enum class Color { RED, GREEN, BLUE };
+enum class Size { SMALL, MEDIUM, LARGE };
 
 struct Product
 {
@@ -48,9 +48,32 @@ struct Product
 //  }
 //};
 
+template <typename T> struct AndSpecification;
+
 template <typename T> struct Specification
 {
   virtual bool is_satisfied(T* item) = 0;
+
+  AndSpecification<T> operator&&(Specification<T>&& other)
+  {
+//    AndSpecification<T>(*this, other);
+    return {*this, other};
+  }
+};
+
+template <typename T>
+struct AndSpecification : Specification<T>
+{
+  Specification<T>& first;
+  Specification<T>& second;
+
+  AndSpecification(Specification<T>& first, Specification<T>& second) : first(
+    first), second(second) {}
+
+  bool is_satisfied(T* item) override
+  {
+    return first.is_satisfied(item) && second.is_satisfied(item);
+  }
 };
 
 template <typename T> struct Filter
@@ -99,33 +122,46 @@ struct ProductFilter : Filter<Product>
 
 int main()
 {
-  auto apple = Product{"apple", Color::Red, Size::Small};
-  auto tree = Product{"tree", Color::Green, Size::Medium};
-  auto house = Product{"house", Color::Blue, Size::Large};
+  auto apple = Product{"apple", Color::RED, Size::SMALL};
+  auto tree = Product{"tree", Color::GREEN, Size::MEDIUM};
+  auto house = Product{"house", Color::BLUE, Size::LARGE};
 
   vector<Product*> items = {&apple, &tree, &house};
 
 //  ProductFilter pf;
-//  for (auto& item : pf.by_color(items, Color::Red))
+//  for (auto& item : pf.by_color(items, Color::RED))
 //    cout << item->name << " has color red" << endl;
 //
-//  for (auto& item : pf.by_size(items, Size::Medium))
+//  for (auto& item : pf.by_size(items, Size::MEDIUM))
 //    cout << item->name << " has size medium" << endl;
 //
-//  for (auto& item : pf.by_color_and_size(items, Color::Blue, Size::Large))
+//  for (auto& item : pf.by_color_and_size(items, Color::BLUE, Size::LARGE))
 //    cout << item->name << " has color blue and size large" << endl;
 
   ProductFilter pf;
-  auto green = ColorSpecification{Color::Green};
-  auto small = SizeSpecification{Size::Small};
+  auto green = ColorSpecification{Color::GREEN};
+  auto medium = SizeSpecification{Size::MEDIUM};
 
   for (auto& item : pf.filter(green, items))
     cout << item->name << " has color green" << endl;
 
-//  for (auto& item : pf.by_size(items, Size::Medium))
+  auto green_and_medium = AndSpecification(green, medium);
+
+  for (auto& item : pf.filter(green_and_medium, items))
+    cout << item->name << " has color green and size medium" << endl;
+
+//  for (auto& item : pf.by_size(items, Size::MEDIUM))
 //    cout << item->name << " has size medium" << endl;
 //
-//  for (auto& item : pf.by_color_and_size(items, Color::Blue, Size::Large))
+//  for (auto& item : pf.by_color_and_size(items, Color::BLUE, Size::LARGE))
 //    cout << item->name << " has color blue and size large" << endl;
+
+  auto blue_and_large = ColorSpecification{Color::BLUE}
+                        && SizeSpecification{Size::LARGE};
+
+  for (auto& item : pf.filter(blue_and_large, items))
+    cout << item->name << " has color blue and size large" << endl;
+
+
   return 0;
 }
